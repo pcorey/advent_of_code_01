7091c1fbc1289e87a1d65b7ec5e1016e60136836
Pete Corey <petecorey@gmail.com>
2016-08-17 07:30:34 -0400

    # Our First Test
    To get things going, let's write the simplest test we can think of.
    Amazingly, tests for Elixir functions can be written within the
    documentation for the function itself. For example, we can write our
    first test like this:
    
        iex> AdventOfCode01.which_floor "("
        1
    
    Elixir will tease these tests out of the function docs and run them as
    part of our test suite.
    
    We can make this first test pass by simply returning 1 from our new
    `which_floor` function:
    
        def which_floor(directions) do
          1
        end
    
    And with that, our test suite flips back to green.

--- lib/advent_of_code_01.ex
+++ lib/advent_of_code_01.ex
@@ -1,2 +1,16 @@
 defmodule AdventOfCode01 do
+
+  @doc """
+  Determines which floor Santa will end up on.
+
+  ## Examples
+
+  iex> AdventOfCode01.which_floor "("
+  1
+
+  """
+  def which_floor(directions) do
+    1
+  end
+
 end
